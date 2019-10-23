d6991ef056379226b824445ed4d1c950a538a4be
Luke Brannigan <lukebrannigan57@gmail.com>
2019-10-23 01:56:44 -0400

    practice branch

--- src/index.js
+++ src/index.js
@@ -9,21 +9,34 @@ const userHand = singleDeckGame.getUserHand(); //gets user hand, probably need o
 generateCard(userHand.getCards()[0]);
 generateCard(userHand.getCards()[1]);  //gets top two cards out of array of cards (Deck)
 
-function generateCard(card) {
+function generateCard(card) {  //generates the card with suit and value
 
-    const playingCard = document.createElement("section");
-    playingCard.classList.add("playing-card");
+    const playingCard = document.createElement("section"); //creates a <section> that will be the 
+    playingCard.classList.add("playing-card");             //actual  playing card
+    
 
-    const valueContainer = document.createElement("section");
-    valueContainer.classList.add("value-container");
-
-    const value = document.createElement("span");
-    value.classList.add("value");
+    const valueContainer = document.createElement("section");  //creates a <section> that will be the 
+    valueContainer.classList.add("value-container");           //element that stores the value and suit
+                                                                //of the card
+                                                                //also variable "valueContainer"
+    const value = document.createElement("span");  //also variable "value"
+    value.classList.add("value");                  //creates a <span> that will hold the card's value
     value.textContent = card.getValue();
 
+    const value = document.createElement("span"); //creates a <span> that will hold the card's suit
+    value.classList.add("suit");                 
+    value.textContent = card.getValue();
 
-// const playingCard = document.playingCard.classList.add("playing-card");
-// const valueContainer
-// valueContainer.classList
+    valueContainer.append(value);           //updates value, suit and valueContainer
+    valueContainer.append(suit);
+    playingCard.append(valueContainer);
+
+    const table = document.querySelector(".table");  //interacts with the css element
+    table.append(playingCard);  //updates playingCard
+
+
+    
+
+
 
 }
